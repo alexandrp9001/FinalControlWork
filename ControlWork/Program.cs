48c6cf0f06a0f20a3122d8ad75c72e7986eb5f14
alexandrp9001 <140640940+alexandrp9001@users.noreply.github.com>
2024-01-07 11:57:41 +0300

    new array

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr = { "KVM", "26", "Kazantip", "ABB", "6" };
+
+string[] numSing(string[] arr)
+{
+  int n = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i].Length <= 3)
+      n++;
+  }
+  string[] result = new string[n];
+  int j = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i].Length <= 3)
+    {
+      result[j] = arr[i];
+      j++;
+    }
+  }
+  return result;
+}
